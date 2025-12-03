fc7e48efe18781becd45b4a4371116e4db9eabec
GGDimitrov21 <GGDimitrov21@codingburgas.bg>
2025-12-04 00:28:48 +0200

    ok wtf

--- api/src/Iara.Application/Abstractions.cs
+++
@@ -1,8 +0,0 @@
-namespace Iara.Application.Abstractions;
-
-using Iara.Domain.Entities;
-
-public interface IVesselRepository
-{
-    Task<Vessel?> GetByIdAsync(int id, CancellationToken ct = default);
-}
